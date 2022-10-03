5eaabdc90ceb907f952b1463b791e4b9ef2b05b9
Salim <salimbabaji@gmail.com>
2022-10-03 06:40:05 +0100

    Added the a game function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 
-const computerChoice = function getCPUchoice() {
+const computerChoice = () => {
     const choices = ["rock", "paper", "scissors"];
 
     const ranNo = Math.random() * 3
@@ -10,43 +10,73 @@ const computerChoice = function getCPUchoice() {
     return choices[ranChoice];
 
 }
+var cpuCount = 0, playerCount = 0;
 
-function round(computer, player) {
+let round = (computer, player) => {
 
 
     ///tie
-    if (computer== player) {
-        console.log("ITS A TIE!!")
+
     
+    if (computer == player) {
+        console.log("ITS A TIE!!")
+
     }
 
     //paper covers rock
     else if (computer == 'rock' && player == 'paper') {
         console.log("You win! Paper covers rock!")
+        playerCount++;
     }
     else if (computer == 'paper' && player == 'rock') {
         console.log("You lose! Paper covers rock!")
+        cpuCount++;
     }
 
     //rock beats scissors
     else if (computer == "rock" && player == "scissors") {
         console.log("You lose! Rock beats scissors!")
+        cpuCount++;
     }
 
     else if (computer == "scissors" && player == "rock") {
         console.log("You win! Rock beats scissors!")
+        playerCount++;
     }
 
     //scissors beats paper
     else if (computer == "scissors" && player == "paper") {
         console.log("You lose! scissors beats paper!")
+        cpuCount++;
     }
 
     else if (computer == "paper" && player == "scissors") {
         console.log("You win! scissors beats paper!")
+        playerCount++
     }
 
 }
 
-round(computerChoice(), 'rock')
+const game = () => {
+    let result = "RESULT: "
+    for (let count = 0; count <= 5; count++) {
+        round(computerChoice(), 'scissors')
+    }
+
+    if (cpuCount > playerCount){
+        console.log(result + "The cpu wins")
+    }
+
+    else if(playerCount>cpuCount){
+        console.log(result + "The player wins")
+    }
+
+    else{
+        console.log(result + "Its a tie")
+    }
+}
+
+console.log(game(5))
+
+
 
